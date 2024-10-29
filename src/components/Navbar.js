dd6b8ba2ec2bb4256c5d42e4ea9e636c9912ee0a
Esther <estherjtae@gmail.com>
2024-10-29 03:36:29 -0700

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="navbar">
       <h1 className="navbar-logo">Tae Portfolio</h1>
       <ul className="navbar-menu">
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/COGS125">Home</Link></li>
         <li><Link to="/projects">Projects</Link></li>
       </ul>
     </nav>
